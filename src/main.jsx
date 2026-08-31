1d8bb55045f352ea5d5bafee03a3aa4205355706
dorukarikoglu-dotcom <dorukarikoglu@gmail.com>
2026-08-31 08:17:20 +0300

    frontier: route stage-zero experiments through React entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {DealflowExperiment,AutoBidExperiment} from './FrontierExperiments.jsx'
+
+const path=window.location.pathname
+const RoutedApp=path==='/frontier/dealflow'||path==='/frontier/dealflow/'?DealflowExperiment:path==='/frontier/autobid'||path==='/frontier/autobid/'?AutoBidExperiment:App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RoutedApp />
   </React.StrictMode>,
 )
