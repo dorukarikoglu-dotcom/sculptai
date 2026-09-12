e261c80a1188d8b0b956ea2d5f40ed3832daa137
dorukarikoglu-dotcom <dorukarikoglu@gmail.com>
2026-09-12 09:51:38 +0300

    Route machinery engine to V2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {DealflowExperiment,AutoBidExperiment} from './FrontierExperiments.jsx'
-import MachineryDealEngine from './MachineryDealEngine.jsx'
+import MachineryDealEngineV2 from './MachineryDealEngineV2.jsx'
 
 const path=window.location.pathname
 const RoutedApp=path==='/frontier/dealflow'||path==='/frontier/dealflow/'
@@ -10,7 +10,7 @@ const RoutedApp=path==='/frontier/dealflow'||path==='/frontier/dealflow/'
   :path==='/frontier/autobid'||path==='/frontier/autobid/'
     ?AutoBidExperiment
     :path==='/machinery-engine'||path==='/machinery-engine/'
-      ?MachineryDealEngine
+      ?MachineryDealEngineV2
       :App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
