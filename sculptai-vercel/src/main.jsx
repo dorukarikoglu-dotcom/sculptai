00f061efac93e6f92ef4ffdb281fd64472264124
dorukarikoglu-dotcom <dorukarikoglu@gmail.com>
2026-09-12 09:54:21 +0300

    Route production machinery engine V2

--- sculptai-vercel/src/main.jsx
+++ sculptai-vercel/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ErrorBoundary } from './App.jsx'
+import App,{ ErrorBoundary } from './App.jsx'
+import MachineryDealEngineV2 from './MachineryDealEngineV2.jsx'
+
+const path=window.location.pathname
+const RoutedApp=path==='/machinery-engine'||path==='/machinery-engine/'?MachineryDealEngineV2:App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
-    <App />
+    <RoutedApp />
   </ErrorBoundary>
 )
