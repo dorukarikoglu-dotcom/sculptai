8f896ebff50298f22d0229d006e716d81c3e5188
dorukarikoglu-dotcom <dorukarikoglu@gmail.com>
2026-09-12 09:48:05 +0300

    Add internal machinery deal engine
    * Add machinery deal engine internal tool
    
    * Route machinery engine internal app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {DealflowExperiment,AutoBidExperiment} from './FrontierExperiments.jsx'
+import MachineryDealEngine from './MachineryDealEngine.jsx'
 
 const path=window.location.pathname
-const RoutedApp=path==='/frontier/dealflow'||path==='/frontier/dealflow/'?DealflowExperiment:path==='/frontier/autobid'||path==='/frontier/autobid/'?AutoBidExperiment:App
+const RoutedApp=path==='/frontier/dealflow'||path==='/frontier/dealflow/'
+  ?DealflowExperiment
+  :path==='/frontier/autobid'||path==='/frontier/autobid/'
+    ?AutoBidExperiment
+    :path==='/machinery-engine'||path==='/machinery-engine/'
+      ?MachineryDealEngine
+      :App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
